b37abef1d8e515b86ff3d8c508b9737ca8b72c03
Grzegorz Gałęzowski <ftw637@motorolasolutions.com>
2020-04-07 08:15:16 +0200

    Added support for globbing

--- TrxConverter/TrxConverter/Program.cs
+++ TrxConverter/TrxConverter/Program.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.IO;
+using GlobExpressions;
 
 namespace TddXt.TrxConverter
 {
-  class Program
-  {
-    static void Main(string[] args)
+    static class Program
     {
-      var text = File.ReadAllText(args[0]);
-      var testRun = text.ParseXml<TestRun>();
-
-      foreach (var result in testRun.Results)
-      {
-        Console.WriteLine("-------------");
-        Console.WriteLine(result.outcome + ": " + result.testName);
-        if (result.Output != null)
+        static void Main(string[] args)
         {
-          if (result.Output.StdOut != null)
-          {
-            Console.WriteLine(result.Output.StdOut);
-          }
-
-          if (result.Output.ErrorInfo != null)
-          {
-            Console.WriteLine(result.Output.ErrorInfo.Message);
-            Console.WriteLine(result.Output.ErrorInfo.StackTrace);
-          }
+            foreach (var trxPath in Glob.Files(args[0], args[1]))
+            {
+                PrintTrx(Path.Combine(args[0], trxPath));
+            }
         }
-      }
+
+        private static void PrintTrx(string path)
+        {
+            var text = File.ReadAllText(path);
+            var testRun = text.ParseXml<TestRun>();
+
+            foreach (var result in testRun.Results)
+            {
+                Console.WriteLine("=============");
+                Console.WriteLine(result.outcome + ": " + result.testName);
+                Console.WriteLine("=============");
+
+                if (result.Output != null)
+                {
+                    if (result.Output.StdOut != null)
+                    {
+                        Console.WriteLine(result.Output.StdOut);
+                    }
+
+                    if (result.Output.ErrorInfo != null)
+                    {
+                        Console.WriteLine(result.Output.ErrorInfo.Message);
+                        Console.WriteLine(result.Output.ErrorInfo.StackTrace);
+                    }
+                }
+            }
+        }
     }
-  }
 }
